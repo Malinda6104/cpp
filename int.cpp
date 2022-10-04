349da7794e262ae93edb3518a239e0217b0cc4eb
Malinda6104 <99778993+Malinda6104@users.noreply.github.com>
2022-10-04 12:48:01 +0530

    Create int.cpp

---
+++ int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int smallest=10; 
+   int largest=100; 
+
+   cout <<"Smallest Number: " << smallest << "\n"; 
+   cout <<"Largest Number: " << largest << "\n";
+   return 0; 
+}
